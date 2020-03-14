c4fc7470e2014173d4582c37c34224a2315b81d5
Neha Yadav <nehayadav0102@gmail.com>
2020-03-14 14:39:33 +0530

    added comments

--- digital_makeup_image_demo.py
+++ digital_makeup_image_demo.py
@@ -7,20 +7,28 @@ import numpy
 jewel_img = cv2.imread("jewelery.png")
 frame = cv2.imread('priyanka.jpeg')
 frame = cv2.resize(frame,(432, 576))
+
+# Returns a list of face landmarks present on frame
 face_landmarks_list = face_recognition.face_landmarks(frame)
+# For demo images only one person is present in image 
 face_landmarks = face_landmarks_list[0]
 
 shape_chin = face_landmarks['chin']
+# x,y cordinates on frame where jewelery will be added
 x = shape_chin[3][0]
 y = shape_chin[6][1]
-
+# Jewelry width & height calculated using face chin cordinates
 img_width = abs ( shape_chin[3][0] - shape_chin[14][0])
-img_height = int( 1.02 * img_width)
+img_height = int( 1.02 * img_width) 
 jewel_img = cv2.resize(jewel_img, (img_width,img_height), interpolation=cv2.INTER_AREA)
 jewel_gray = cv2.cvtColor(jewel_img, cv2.COLOR_BGR2GRAY)
+# All pixels greater than 230 will be converted to white and others will be converted to black
 thresh, jewel_mask = cv2.threshold(jewel_gray, 230, 255, cv2.THRESH_BINARY)
+# Convert to black the background of jewelry image
 jewel_img[jewel_mask == 255] = 0
+# Crop out jewelry area from original frame
 jewel_area = frame[y:y+img_height, x:x+img_width]
+# bitwise_and will convert all black regions in any image to black in resulting image
 masked_jewel_area = cv2.bitwise_and(jewel_area, jewel_area, mask=jewel_mask)
 final_jewel = cv2.add(masked_jewel_area, jewel_img)
 
